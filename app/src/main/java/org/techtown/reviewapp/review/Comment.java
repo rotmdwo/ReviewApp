719d84e5b9f0bd0bc766edc3c744619e0e14967a
이청수 <bestowing@naver.com>
2020-02-03 17:07:46 +0900

    추가: 리뷰, 댓글 클래스 추가

---
+++ app/src/main/java/org/techtown/reviewapp/review/Comment.java
@@ -0,0 +1,40 @@
+package org.techtown.reviewapp.review;
+
+public class Comment {
+    //클래스 안의 변수는 카멜 케이스가 아닌 _로 구분.
+    String user_id;
+    String user_nickname;
+    String comment_text;
+    String date;
+
+    //생성자
+    public Comment(String user_id, String comment_text, String date) {
+        this.user_id = user_id;
+        this.comment_text = comment_text;
+        this.date = date;
+    }
+
+    public String get_user_id() {
+        return user_id;
+    }
+
+    public void set_user_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String get_comment_text() {
+        return comment_text;
+    }
+
+    public void set_comment_text(String comment_text) {
+        this.comment_text = comment_text;
+    }
+
+    public String get_date() {
+        return date;
+    }
+
+    public void set_date(String date) {
+        this.date = date;
+    }
+}
---
+++ app/src/main/java/org/techtown/reviewapp/review/Review.java
@@ -0,0 +1,15 @@
+package org.techtown.reviewapp.review;
+
+import java.util.ArrayList;
+
+public class Review {
+    ArrayList<Comment> comments = new ArrayList<>();
+    String user_id;
+    String user_nickname;
+    String user_rank;
+    String restaurant;
+    String date;
+    String user_text;
+    int like;
+
+}
