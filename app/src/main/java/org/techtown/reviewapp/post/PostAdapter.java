9944f7c9d69f4d5cfe9df6855ddea505be1a8919
rotmdwo <49435944+rotmdwo@users.noreply.github.com>
2020-02-08 13:45:41 +0900

    댓글도 시간표기 적용

--- app/src/main/java/org/techtown/reviewapp/post/PostAdapter.java
+++ app/src/main/java/org/techtown/reviewapp/post/PostAdapter.java
@@ -101,8 +101,38 @@ public class PostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
             //Do your stuff here
             Post post = posts.get(position);
             user_nickname.setText(post.getUser_nickname());
-            date.setText(post.getDate());
             user_text.setText(post.getUser_text());
+
+            Date date1 = new Date();
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+            String current_date = format.format(date1);
+            String then_date = post.getDate();
+            int current_year = Integer.parseInt(current_date.substring(0,4));
+            int then_year = Integer.parseInt(then_date.substring(0,4));
+            int current_month = Integer.parseInt(current_date.substring(5,7));
+            int then_month = Integer.parseInt(then_date.substring(5,7));
+            int current_day = Integer.parseInt(current_date.substring(8,10));
+            int then_day = Integer.parseInt(then_date.substring(8,10));
+            int current_hour = Integer.parseInt(current_date.substring(11,13));
+            int then_hour = Integer.parseInt(then_date.substring(11,13));
+            int current_minute = Integer.parseInt(current_date.substring(14,16));
+            int then_minute = Integer.parseInt(then_date.substring(14,16));
+            int current_second = Integer.parseInt(current_date.substring(17,19));
+            int then_second = Integer.parseInt(then_date.substring(17,19));
+
+            if((current_year-then_year>=2) || (current_year - then_year == 1 && current_month - then_month >= 0)){
+                date.setText(current_year-then_year+"년 전");
+            } else if((current_month-then_month>=2) || (current_month - then_month == 1 && current_day - then_day >= 0)){
+                date.setText(current_month-then_month+"달 전");
+            } else if((current_day-then_day>=2) || (current_day - then_day == 1 && current_hour - then_hour >= 0)){
+                date.setText(current_day-then_day+"일 전");
+            } else if((current_hour-then_hour>=2) || (current_hour - then_hour == 1 && current_minute - then_minute >= 0)){
+                date.setText(current_hour-then_hour+"시간 전");
+            } else if((current_minute-then_minute>=2) || (current_minute - then_minute == 1 && current_second - then_second >= 0)){
+                date.setText(current_minute-then_minute+"분 전");
+            } else{
+                date.setText("방금 전");
+            }
         }
     }
 
