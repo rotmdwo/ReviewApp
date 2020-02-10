fb9c9a5d508acdce0c19a92cc5a9a9332a6086b6
rotmdwo <49435944+rotmdwo@users.noreply.github.com>
2020-02-10 23:50:37 +0900

    Status Fragment가 떴을 때 네비게이션 버튼 및 뒷배경 프래그먼트 비활성화

--- app/src/main/java/org/techtown/reviewapp/home/HomeActivity.java
+++ app/src/main/java/org/techtown/reviewapp/home/HomeActivity.java
@@ -2,6 +2,7 @@ package org.techtown.reviewapp.home;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
