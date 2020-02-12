a8cba044b8102877984a7212d92749ea67bb07cb
이청수 <bestowing@naver.com>
2020-02-12 14:58:49 +0900

    추가: 레스토랑 액티비티 및 xml 파일 생성

---
+++ app/src/main/java/org/techtown/reviewapp/Restaurants/RestaurantActivity.java
@@ -0,0 +1,18 @@
+package org.techtown.reviewapp.Restaurants;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.techtown.reviewapp.R;
+
+public class RestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+
+
+    }
+}
