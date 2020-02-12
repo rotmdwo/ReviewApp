1c4b4b50fa311dac4c5ea729edf340a25264c828
이청수 <bestowing@naver.com>
2020-02-12 18:12:22 +0900

    추가: 음식점 관련 프래그먼트와 xml 파일 추가

--- app/src/main/java/org/techtown/reviewapp/Restaurants/RestaurantActivity.java
+++ app/src/main/java/org/techtown/reviewapp/Restaurants/RestaurantActivity.java
@@ -1,18 +1,66 @@
 package org.techtown.reviewapp.Restaurants;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.techtown.reviewapp.R;
+import org.techtown.reviewapp.Restaurants.restaurant.InfoFragment;
+import org.techtown.reviewapp.Restaurants.restaurant.TimeLineFragment;
 
 public class RestaurantActivity extends AppCompatActivity {
+    FragmentManager manager;
+    InfoFragment infoFragment;
+    TimeLineFragment timeLineFragment;
+    ImageView prev, option;
+    TextView time_line, resto_info;
+    public static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
 
+        manager = getSupportFragmentManager();
+        mContext = this;
 
+        prev = findViewById(R.id.prev);
+        option = findViewById(R.id.option);
+        time_line = findViewById(R.id.time_line);
+        resto_info = findViewById(R.id.resto_info);
+
+        infoFragment = new InfoFragment();
+        timeLineFragment = new TimeLineFragment();
+        manager.beginTransaction()
+                .add(R.id.frameLayout, timeLineFragment)
+                .add(R.id.frameLayout, infoFragment).hide(infoFragment)
+                .commit();
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        time_line.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                manager.beginTransaction().hide(infoFragment).show(timeLineFragment).commit();
+            }
+        });
+
+        resto_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                manager.beginTransaction().hide(timeLineFragment).show(infoFragment).commit();
+            }
+        });
     }
 }
---
+++ app/src/main/java/org/techtown/reviewapp/Restaurants/restaurant/InfoFragment.java
@@ -0,0 +1,32 @@
+package org.techtown.reviewapp.Restaurants.restaurant;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.techtown.reviewapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InfoFragment extends Fragment {
+
+
+    public InfoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+
+}
