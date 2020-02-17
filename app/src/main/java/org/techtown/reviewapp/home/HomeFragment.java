379eb68a5aeb819544773e694b584ed0cfe19649
rotmdwo <49435944+rotmdwo@users.noreply.github.com>
2020-02-17 23:14:02 +0900

    아래로 내릴 때 추가 글 가져오기 기능 추가

--- app/src/main/java/org/techtown/reviewapp/home/HomeFragment.java
+++ app/src/main/java/org/techtown/reviewapp/home/HomeFragment.java
@@ -1,8 +1,11 @@
 package org.techtown.reviewapp.home;
 
 
+import android.annotation.TargetApi;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +13,11 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -48,6 +54,9 @@ public class HomeFragment extends Fragment implements PostAdapter.ItemAddListene
     String upload_num;
     boolean isAdded = false;
     public static HomeFragment mContext;
+    int current_page = 1;
+    ProgressBar progressBar;
+    Boolean loadedAll = false;
 
 
     @Override
@@ -57,6 +66,8 @@ public class HomeFragment extends Fragment implements PostAdapter.ItemAddListene
         //((HomeActivity) HomeActivity.mContext).home.setScaleType(ImageView.ScaleType.FIT_CENTER);
         final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
         mContext = this;
+
+        progressBar = rootView.findViewById(R.id.progressBar);
 
         swipeRefreshLayout = rootView.findViewById(R.id.swipeLayout);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -69,6 +80,10 @@ public class HomeFragment extends Fragment implements PostAdapter.ItemAddListene
             }
         });
 
+
+
+
+
         reference.addValueEventListener(dataListener);
         postAdapter = new PostAdapter(getContext());
         postAdapter.itemAddListener = this;
@@ -79,6 +94,18 @@ public class HomeFragment extends Fragment implements PostAdapter.ItemAddListene
         manager.setStackFromEnd(true);
         manager.setItemPrefetchEnabled(true);  // 리사이클러뷰 정보 미리 불러오기
         recyclerView.setLayoutManager(manager);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener(){
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {  // 리사이클러뷰의 끝에 다면
+                super.onScrolled(recyclerView, dx, dy);
+                if(recyclerView.computeVerticalScrollOffset() + recyclerView.computeVerticalScrollExtent() >= recyclerView.computeVerticalScrollRange()){
+                    progressBar.setVisibility(View.VISIBLE);
+                    reference.addListenerForSingleValueEvent(dataListener4);
+                }
+
+            }
+        });
 
         ImageButton imageButton = rootView.findViewById(R.id.imageButton);  // 포스트 버튼
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -121,6 +148,8 @@ public class HomeFragment extends Fragment implements PostAdapter.ItemAddListene
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     //최근 10개 게시물에 대해 반복
                     for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+
                         //게시물에 필요한 변수
                         String id;
                         String date;
@@ -375,4 +404,141 @@ public class HomeFragment extends Fragment implements PostAdapter.ItemAddListene
                     commit();
         }
     }
+
+    ValueEventListener dataListener4 = new ValueEventListener() {
+
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            Boolean isAlreadyTriggered = false;
+            Query query1 = reference.child("Status").limitToLast((current_page+1)*10);
+            //디비 안의 내용이 바뀔때마다 실행되는 구조임, 최초에만 실행되게 변경
+            if(isAlreadyTriggered == true) {
+                return;
+            } else{
+                isAlreadyTriggered = true;
+                //먼저 유저 정보를 dataSnapshot2에 담음
+                final DataSnapshot dataSnapshot2 = dataSnapshot.child("user");
+                //쿼리문: status에서 최근 10개 게시물을 불러옴
+                query1.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        int all_posts = 0;
+                        long limit = dataSnapshot.getChildrenCount();
+                        int num = 0;
+                        int crop_num = 0;
+
+                        if(limit == 0){
+                            crop_num = 0;
+                        } else if(limit%10==0){
+                            crop_num = 10;
+                        } else{
+                            crop_num = (int)limit%10;
+                        }
+                        //최근 10개 게시물에 대해 반복
+                        for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                            if(num >= crop_num || loadedAll == true){
+                                if(limit!=0 && limit%10 == 0){
+                                    current_page++;
+                                } else{
+                                    loadedAll = true;
+                                }
+                                progressBar.setVisibility(View.INVISIBLE);
+                                break;
+                            }
+                            //게시물에 필요한 변수
+                            String id;
+                            String date;
+                            int like;
+                            String picture;
+                            String restaurant;
+                            String text;
+                            String user_num;
+                            String nickname;
+
+                            //게시물의 키값
+                            String DB_key = snapshot.getKey();
+                            //게시물 밑의 댓글에 대한 정보: message_comment
+                            Map<String, Object> message_comment = (Map<String, Object>) snapshot.child("comments").getValue();
+                            int comment_num = (int) snapshot.child("comments").getChildrenCount() -1;
+                            //댓글 추가
+                            for(DataSnapshot dataSnapshot1 : snapshot.child("comments").getChildren()) {
+                                if(dataSnapshot1.getKey().equals("num")) {
+                                    continue;
+                                } else {
+                                    Map<String, Object> message_comment2 = (Map<String, Object>) dataSnapshot1.getValue();
+                                    date = (String) message_comment2.get("date");
+                                    id = (String) message_comment2.get("id");
+                                    text = (String) message_comment2.get("text");
+                                    user_num = message_comment2.get("user_num").toString();;
+                                    Map<String, Object> message_user2 = (Map<String, Object>) dataSnapshot2.child(user_num).getValue();
+                                    nickname = (String) message_user2.get("nickname");
+
+                                    Post comment = new Post();
+                                    comment.setComment(id, nickname, date, text, 2, dataSnapshot1.getKey());
+                                    postAdapter.addItem(comment,num++);
+                                    crop_num++;
+                                    all_posts++;
+                                }
+                            }
+                            Log.d("asdf","e");
+                            //게시물에 대한 정보: message_post
+                            //id, date, like, picture, restaurant, text, user_num
+                            Map<String, Object> message_post = (Map<String, Object>) snapshot.getValue();
+                            id = (String) message_post.get("id");
+                            date = (String) message_post.get("date");
+                            like = Integer.parseInt(message_post.get("like").toString());
+                            picture = (String) message_post.get("picture");
+                            restaurant = (String) message_post.get("restaurant");
+                            text = (String) message_post.get("text");
+                            user_num = message_post.get("user_num").toString();
+                            //사진이 여러개일 때를 대비하여 ArrayList 선언
+                            ArrayList<String> photo = new ArrayList<>();
+                            photo.add(picture);
+
+                            //게시물을 작성한 유저에 대한 정보: message_user
+                            //nickname, level
+                            //현재 user_num으로 찾음
+                            //(주: 굳이 user_num으로 찾을 필요가 없음, id는 유니크하기 때문에 구조를 바꿀 필요가 있을듯)
+                            Map<String, Object> message_user = (Map<String, Object>) dataSnapshot2.child(user_num).getValue();
+                            nickname = (String) message_user.get("nickname");
+                            int level = Integer.parseInt(message_user.get("level").toString());
+
+                            //받아온 정보를 바탕으로 새로운 Post를 만든다
+                            if (picture.equals("NO")) {
+                                Post status = new Post();
+                                status.setComment_num(comment_num);
+                                status.setStatus(id, nickname, date, text, 1, "레벨 " + level, restaurant, like, DB_key);
+                                postAdapter.addItem(status,num);
+                            } else {
+                                Post review = new Post();
+                                review.setComment_num(comment_num);
+                                review.setReview(id, nickname, date, text, 0, "레벨 " + level, restaurant, like, DB_key, photo, 1);
+                                postAdapter.addItem(review,num);
+                            }
+                            all_posts++;
+                            //어댑터에 반영
+                            recyclerView.setAdapter(postAdapter);
+                            recyclerView.scrollToPosition(all_posts);
+                            num++;
+                        }
+
+
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
 }
